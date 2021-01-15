eb329ae5d068e827c15c5272e1a5fb48936fe344
yunnysunny <yunnysunny@gmail.com>
2021-01-15 17:36:12 +0800

    使用es2018语法

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "webextensions":true,
-        "node": true
+    'env': {
+        'browser': true,
+        'es6': true,
+        'webextensions':true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': [
+            'error',
             4
         ],
         // "linebreak-style": [
         //     "error",
         //     "windows"
         // ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ],
-		"no-console": 0
+        'no-console': 2
     }
 };
