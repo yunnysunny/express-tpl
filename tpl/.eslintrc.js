a0a78a124872758d790de8f06591a240d3e8c34a
yunnysunny <yunnysunny@gmail.com>
2021-03-15 11:24:08 +0800

    更新eslint配置

--- tpl/.eslintrc.js
+++ tpl/.eslintrc.js
@@ -1,30 +1,35 @@
 module.exports = {
-    "env": {
-        "mocha": true,
-        "es6": true,
-        "node": true
+    'env': {
+        'mocha': true,
+        'es6': true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 2018
     },
-    "rules": {
-        "indent": [
-            "error",
+    'rules': {
+        'indent': [
+            'error',
             4
         ],
+        'max-len': ['warn', 120],
+        'no-restricted-syntax': ['error', 'WithStatement'],
+        'no-use-before-define': ['error', { functions: false, classes: true }],
+        'space-before-function-paren': ['error', 'never'],
+        'one-var': ['error', {'const': 'never', 'let': 'never'}],
         // "linebreak-style": [
         //     "error",
         //     "windows"
         // ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ],
-        "no-console": 2
+        'no-console': 2
     }
 };
